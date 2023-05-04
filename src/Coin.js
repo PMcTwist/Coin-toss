5a45f209d4e90cf54181379e4cad40690676e929
P@ <patmaynard452@gmail.com>
2023-05-04 13:43:07 -0400

    Added Coin.css and class to coin object
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- src/Coin.js
+++ src/Coin.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Coin extends Component {
     render() {
         return(
-            <div>
+            <div className='Coin'>
                 <h2>{this.props.info.side}</h2>
                 <img src={this.props.info.img} alt={this.props.info.side} />
             </div>
