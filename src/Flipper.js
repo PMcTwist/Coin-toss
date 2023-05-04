3c6d020103efb993e124fe47c7a711293bf9e1a1
P@ <patmaynard452@gmail.com>
2023-05-04 13:33:31 -0400

    -added in the event middle method -changed how the state was being accessed so it would work properly -imported the png files to be used as the images -Added a counter for each outcome and a full count of flips
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- src/Flipper.js
+++ src/Flipper.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import Coin from './Coin';
 
+import tails from './tails.png';
+import heads from './heads.png';
+
 class Flipper extends Component {
+    static defaultProps = {
+        coin: [
+            { side: 'heads', img: heads },
+            { side: 'tails', img: tails },
+        ]
+    };
+
     constructor(props) {
         super(props);
-        this.state = {side: 'heads'};
-        this.tosser = this.tosser.bind(this);
+        this.state = {
+            currCoin: null,
+            numFlips: 0,
+            numHeads: 0,
+            numTails: 0
+        };
+        this.clicked = this.clicked.bind(this);
     };
 
     // flip function to update side state
     tosser() {
-        let flip = ''
-        {(Math.round(Math.random()) === 1) ? flip = 'heads' : flip = 'tails' } 
-        this.setState({side: flip});
+        const flip = Math.round(Math.random())
+        const flippedCoin = this.props.coin[flip]
+
+        this.setState(currState => {
+            return{
+                currCoin: flippedCoin,
+                numFlips: currState.numFlips + 1,
+                numHeads: currState.numHeads + ((flippedCoin.side === 'heads') ? 1 : 0),
+                numTails: currState.numTails + ((flippedCoin.side === 'tails') ? 1 : 0)
+            }
+        });
+    };
+
+    // Grab the click event
+    clicked(e) {
+        this.tosser();
     };
 
     // render method to return the object
@@ -20,8 +48,16 @@ class Flipper extends Component {
         return(
             <div>
                 <h1>Toss a Coin!</h1>
-                <Coin upSide={this.state.side}/>
-                <button onClick={this.tosser}>Toss!</button>
+
+                {this.state.currCoin  && <Coin info={ this.state.currCoin }/>}
+
+                <button onClick={this.clicked}>Toss!</button>
+
+                <p>
+                    Flips: { this.state.numFlips }<br/>
+                    Heads: { this.state.numHeads }<br/>
+                    Tails: { this.state.numTails }<br/>
+                </p>
             </div>
         )
     };
