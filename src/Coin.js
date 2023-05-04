2e49f95ac71d6118f137cc13268689ecebe49998
P@ <patmaynard452@gmail.com>
2023-05-04 13:32:35 -0400

    Added an h2 tag to say what the outcome was added in the info pass in to give the img some props
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- src/Coin.js
+++ src/Coin.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Coin extends Component {
     render() {
         return(
-            <h1>Coin!</h1>
+            <div>
+                <h2>{this.props.info.side}</h2>
+                <img src={this.props.info.img} alt={this.props.info.side} />
+            </div>
         )
     }
 };
